fa087d596bad0fa600f4f551ebcb75e42f5ce3c1
strippo <marcodicesare72@gmail.com>
2021-01-12 11:21:58 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PS3='Please enter your choice: '
-options=("Russian" "Quit")
+options=("Russian" "Italiano" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -9,6 +9,12 @@ do
             cd langs
             chmod +x Russian.sh
             ./Russian.sh
+            break 
+            ;; 
+            "Italiano")
+            cd langs
+            chmod +x Italian.sh
+            ./Italian.sh
             break 
             ;; 
         "Quit")
